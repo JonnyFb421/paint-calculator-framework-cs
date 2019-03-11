ab68db942e1505120f455fa32483165afbc38b4c
Jonny Carlyon <rr828690@fincoad.com>
2019-03-11 11:17:03 -0500

    Initial commit

---
+++ PaintCalculatorAutomation/Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using OpenQA.Selenium;
+
+namespace PaintCalculatorAutomation
+{
+    using PaintCalculatorAutomation.Utilities;
+    public class UnitTest1
+    {
+        private readonly IWebDriver driver = DriverHelper.GetDriver();
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ PaintCalculatorAutomation/Utilities/DriverHelper.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace PaintCalculatorAutomation.Utilities
+{
+    public abstract class DriverHelper
+    {
+        public static IWebDriver GetDriver()
+        {
+            ChromeDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("http://paint-calc.com/");
+            return driver;
+        }
+    }
+}
